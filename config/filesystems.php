a0dbe547f23cb030403d6f077e052fb93b8ea0a8
Michael Aguiar <mike@aliasproject.com>
2025-07-02 17:09:12 -0700

    filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
@@ -13,7 +12,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    "default" => env("FILESYSTEM_DISK", "local"),
 
     /*
     |--------------------------------------------------------------------------
@@ -28,38 +27,53 @@ return [
     |
     */
 
-    'disks' => [
-
-        'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
-            'throw' => false,
-            'report' => false,
+    "disks" => [
+        "local" => [
+            "driver" => "local",
+            "root" => storage_path("app/private"),
+            "serve" => true,
+            "throw" => false,
+            "report" => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
+        "public" => [
+            "driver" => "local",
+            "root" => storage_path("app/public"),
+            "url" => env("APP_URL") . "/storage",
+            "visibility" => "public",
+            "throw" => false,
+            "report" => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
+        "s3" => [
+            "driver" => "s3",
+            "key" => env("AWS_ACCESS_KEY_ID"),
+            "secret" => env("AWS_SECRET_ACCESS_KEY"),
+            "region" => env("AWS_DEFAULT_REGION"),
+            "bucket" => env("AWS_BUCKET"),
+            "url" => env("AWS_URL"),
+            "endpoint" => env("AWS_ENDPOINT"),
+            "use_path_style_endpoint" => env(
+                "AWS_USE_PATH_STYLE_ENDPOINT",
+                false
+            ),
+            "throw" => false,
+            "report" => false,
         ],
 
+        "gcs" => [
+            "driver" => "gcs",
+            "key_file_path" => env("GOOGLE_CLOUD_KEY_FILE", null), // optional: /path/to/service-account.json
+            "key_file" => [], // optional: Array of data that substitutes the .json file (see below)
+            "project_id" => env("GOOGLE_CLOUD_PROJECT_ID", "your-project-id"), // optional: is included in key file
+            "bucket" => env("GOOGLE_CLOUD_STORAGE_BUCKET", "your-bucket"),
+            "path_prefix" => env("GOOGLE_CLOUD_STORAGE_PATH_PREFIX", ""), // optional: /default/path/to/apply/in/bucket
+            "storage_api_uri" => env("GOOGLE_CLOUD_STORAGE_API_URI", null), // see: Public URLs below
+            "api_endpoint" => env("GOOGLE_CLOUD_STORAGE_API_ENDPOINT", null), // set storageClient apiEndpoint
+            "visibility" => "public", // optional: public|private
+            "visibility_handler" => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
+            "metadata" => ["cacheControl" => "public,max-age=86400"], // optional: default metadata
+        ],
     ],
 
     /*
@@ -73,8 +87,7 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
+    "links" => [
+        public_path("storage") => storage_path("app/public"),
     ],
-
 ];
