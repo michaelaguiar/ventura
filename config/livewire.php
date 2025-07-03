88a85061fccec87596f4e0e7bd62d794ccf133f0
Michael Aguiar <mike@aliasproject.com>
2025-07-02 17:30:50 -0700

    config

--- config/livewire.php
+++ config/livewire.php
@@ -64,7 +64,7 @@ return [
 
     "temporary_file_upload" => [
         "disk" => "gcs", // Example: 'local', 's3'              | Default: 'default'
-        "rules" => null, // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        "rules" => ["required", "file", "max:122880"], // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         "directory" => null, // Example: 'tmp'                      | Default: 'livewire-tmp'
         "middleware" => null, // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         "preview_mimes" => [
@@ -87,7 +87,7 @@ return [
             "wma",
         ],
         "max_upload_time" => 5, // Max duration (in minutes) before an upload is invalidated...
-        "cleanup" => true, // Should cleanup temporary uploads older than 24 hrs...
+        "cleanup" => true,
     ],
 
     /*
