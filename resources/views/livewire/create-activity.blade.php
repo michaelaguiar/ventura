defd82f995b74402e723c47985dc183b7e181af6
Michael Aguiar <mike@aliasproject.com>
2025-07-10 09:30:49 -0700

    duplicate icon

--- resources/views/livewire/create-activity.blade.php
+++ resources/views/livewire/create-activity.blade.php
@@ -61,7 +61,7 @@
                             <div class="flex flex-col items-center">
                                 <a href="/community/{{ $community->id }}/vendors" class="bg-[#72d0df] rounded-lg flex items-center justify-center p-3 hover:bg-[#5bc5d6] transition-colors w-full h-12">
                                     <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                                     </svg>
                                 </a>
                                 <span class="text-[9px] text-gray-700 font-medium mt-1">VENDORS</span>
